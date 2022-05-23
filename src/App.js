68140112a6e1bc8272190e3e30848ddcd54826d0
unknown <chafikaim@gmail.com>
2022-05-23 23:41:08 +0200

    ajout du fichier d edition d un pokemons

--- src/App.js
+++ src/App.js
@@ -2,18 +2,19 @@
 import './App.css';
 import {Routes,Route} from 'react-router-dom';
 import Home from './pages/home';
-import Detail from './componenet/detail'
+import Detail from './componenet/detail.jsx'
+import Edite from './componenet/edite.jsx'
 import Navbar from "./componenet/navbar.jsx";
 function App() {
   return (
     <div className="container">
-    <Navbar/>    
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/detail/:id' element={<Detail/>}/>
-    </Routes>
+      <Navbar/>    
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/detail/:id' element={<Detail/>}/>
+        <Route path='/edite/:id' element={<Edite/>}/>
+      </Routes>
     </div>
   );
 }
-
 export default App;
--- src/componenet/helpers/hepl.js
+++ src/componenet/helpers/hepl.js
@@ -1,15 +1,25 @@
 export default class Helpserver{
 
   static getPokemens (){
-     return fetch('http://localhost:3001/pokemons')
+     return (fetch('http://localhost:3001/pokemons')
       .then(respanse => respanse.json())
       .then(data => data)
-      .catch(e=>console.error(e))
+      .catch(e=>console.error(e)))
   }
   static getPokemensByid(id){
-      return fetch(`http://localhost:3001/pokemons/${id}`)
+      return (fetch(`http://localhost:3001/pokemons/${id}`)
       .then(respense=> respense.json())
       .then(data => data)
-      .catch(e=> console.error(e));
+      .catch(e=> console.error(e)));
+  }
+  static setPokemonbyId(pokemons){
+  let newPokemon=pokemons;
+
+    return (fetch(`http://localhost:3001/pokemons/${pokemons.id}`,{
+     method:'Put',
+     body:newPokemon,
+     headers:{'content-types':'application-json'}
+   }))
+
   }
 }
